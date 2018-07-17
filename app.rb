3df76e7ef8cc63303296f6601d61e504fca9952e
Daksha Patel <dakshapatel100@gmail.com>
2018-07-17 22:09:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,10 +8,18 @@ class App < Sinatra::Base
   end
 
   get "/team" do
-    erb :team
+    @team = ['']
   end
 
-  post "/team/" do
+  post "/team" do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    erb :tream
 
   end
 
